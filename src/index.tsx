2b44c45841d5d3221f896f278604d0181fa5a88b
caglar.durmus <caglardur@gmail.com>
2022-02-25 21:55:42 +0300

    typescript training

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import ReactDOM from "react-dom"
 
+import GuestList from "./state/GuestList"
+import UserSearch from "./state/UserSearch"
+import EventComponent from "./events/EventComponent"
+
 const App = () => {
   return (
     <div>
-      <h1>Hi</h1>
+      <EventComponent />
     </div>
   )
 }
